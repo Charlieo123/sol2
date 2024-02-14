0ad377a7639cdfe7041691165088c6692add1e3c
Charlieo123 <99101639+Charlieo123@users.noreply.github.com>
2024-02-14 20:01:55 +0000

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,21 @@
+import Providers from "./providers";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Litter",
+  description: "The latent social network",
+};
+
+export default function RootLayout({ children, params: { session } }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers session={session}>{children}</Providers>
+      </body>
+    </html>
+  );
+}
